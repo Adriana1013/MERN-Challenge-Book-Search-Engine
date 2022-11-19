6969f2984b8e6ebbf5bc3e508986d197b4a5bd12
Adriana Villegas <villegas.a102@gmail.com>
2022-11-19 01:14:39 -0600

    fixing errores in the schema,typeDefs and server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs } = require('./schema/typeDefs')
-const { resolvers } = require('./schema/resolvers')
+const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
